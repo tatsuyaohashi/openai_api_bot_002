6e1b0a965e8497b2d7c9a0baad671e9867375acb
tatsuyaohashi <139756736+tatsuyaohashi@users.noreply.github.com>
2024-07-19 17:37:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-
 import streamlit as st
 import openai
 
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
-openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+openai.api_key = st.secrets["openai_api_key"]
 
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
-        ]
+    ]
 
 # チャットボットとやりとりする関数
 def communicate():
@@ -21,15 +20,14 @@ def communicate():
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages
-    )  
+    )
 
     bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
 
     st.session_state["user_input"] = ""  # 入力欄を消去
 
-
-# ユーザーインターフェイスの構築
+# ユーザーインターフェースの構築
 st.title("My AI Assistant")
 st.write("ChatGPT APIを使ったチャットボットです。")
 
@@ -40,7 +38,8 @@ if st.session_state["messages"]:
 
     for message in reversed(messages[1:]):  # 直近のメッセージを上に
         speaker = "🙂"
-        if message["role"]=="assistant":
-            speaker="🤖"
+        if message["role"] == "assistant":
+            speaker = "🤖"
 
         st.write(speaker + ": " + message["content"])
+
